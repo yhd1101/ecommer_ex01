788a96f135ac4a7516ecd3d562e17be7bcc3f0c3
yhd1101 <asd159369@naver.com>
2023-03-17 01:54:45 +0900

    ecommer_database_review02

--- route/product.js
+++ route/product.js
@@ -4,10 +4,45 @@ import productModel from "../models/product.js";
 const router = express.Router()
 //product를 등록하는 api
 router.get("/", (req, res) => {
-    res.json({
-        msg : "product get all"
-    })
+    productModel
+        .find()
+        .then(product =>{
+            res.json({
+                msg : "get all product",
+                count : product.length,
+                product : product
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg : err.message
+            })
+        })
+
 })
+
+//특정한 product를 불러오는 api
+router.get("/:productid", (req, res) =>{
+    productModel
+        .findById(req.params.productid)
+        .then(product => {
+            if(!product){
+                return res.json({
+                    msg : "no data" //??
+                })
+            }
+            res.json({
+                msg : "Sucessfull get product",
+                product : product
+            })
+        })
+        .catch(err =>{
+            res.json({
+                msg : err.message
+            })
+        })
+})
+
 //product를 등록하는 api
 router.post("/create", (req, res) =>{
     // const newProduct ={
@@ -24,9 +59,15 @@ router.post("/create", (req, res) =>{
     })
     newProduct
         .save() //저장
-        .then(resul =>{
+        .then(result =>{
             res.json({
-                msg : "Successful create user"
+                msg : "Successful create use",
+                user : {
+                    name : result.name,
+                    price : result.price,
+                    result : result._id
+
+                }
             })
         })
         .catch(err =>{
@@ -42,10 +83,32 @@ router.put("/update", (req, res) =>{
     })
 })
 
-router.delete("/delete", (req, res) => {
-    res.json({
-        msg : "deleted a product"
-    })
+
+//product 전체를 삭제하는 api
+router.delete("/", (req, res) => {
+    productModel
+        .deleteMany()
+        .then()
+        .catch(err =>{
+            res.json({
+                msg : err.message
+            })
+        })
+})
+//특정 product를 삭제하는 api
+router.delete("/:productid", (req, res) =>{
+    productModel
+        .findByIdAndDelete(req.params.productid)
+        .then(_ =>{
+            res.json({
+                msg : "deleted a products"
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg : err.message
+            })
+        })
 })
 
 
